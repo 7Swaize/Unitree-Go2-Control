18f456023c24387a54596ffa53ca5195b6ca6bca
7Swaize <rahejasachit@gmail.com>
2026-01-12 22:22:33 -0500

    name change

--- unitree_control/dds/dds_constants.py
+++
@@ -1,7 +0,0 @@
-DDS_TOPICS = {
-    "LOW_STATE": "rt/lf/lowstate",
-    "RAW_LIDAR": "rt/utlidar/cloud_deskewed",
-    "ODOMETRY_LIDAR": "rt/utlidar/cloud",
-    "HEIGHT_MAP_ARRAY": "rt/utlidar/height_map_array",
-    "RANGE_INFO": "rt/utlidar/range_info"
-}
