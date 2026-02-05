e7c0d9016e1f1619ac130c9a681fb0b3f4121c0b
Sachit R <rahejasachit@gmail.com>
2026-02-05 13:58:44 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -xe
 
+DIR=$(dirname "${BASH_SOURCE[0]}")
 
+bash "$DIR/build_realsense.sh"
+bash "$DIR/build_unitree_sdk.sh"
+
+echo -e "\e[92m\n\e[1m Installed all native dependencies. \n\e[0m"
+echo "Next steps:"
+echo "  1. Reboot system"
+echo "  2. Plug in RealSense camera"
+echo "  3. Test with: realsense-viewer"
